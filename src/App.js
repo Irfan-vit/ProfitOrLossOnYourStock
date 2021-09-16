5103524caab5d8070811007ae56e41f823595acc
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2021-09-16 06:53:54 +0530

    input fixed

--- src/App.js
+++ src/App.js
@@ -8,43 +8,40 @@ import sadbackground from "./sadbg.gif";
 import happpybackground from "./blackBg.jpg";
 import nothing from "./nothing.gif";
 export default function App() {
-  var [purchase, setPurchase] = useState();
+  var [purchase, setPurchase] = useState("");
   var [quantity, setQuantity] = useState();
   var [current, setCurrent] = useState();
-  var [output, setOutput] = useState("Please Input all the values for calculating your stocks Profit/Loss");
+  var [output, setOutput] = useState(
+    "Please Input all the values for calculating your stocks Profit/Loss"
+  );
   var [graphics, setGraphics] = useState();
   var [theme, setTheme] = useState();
-  var [btnChk, setBtnChk] = useState(false);
   function getPurchasePrice(event) {
     purchase = event.target.value;
-    if(purchase <= 0 || !purchase) {
-      // alert("Please Enter correct values" + purchase + ": is not accaptable");
-      setOutput("Please Enter correct values");
-    } else {
-      setPurchase(purchase);
-    }
+    setPurchase(purchase);
   }
   function getPurchaseQuantity(event) {
     quantity = event.target.value;
-    if(quantity <= 0 || !quantity) {
-      setOutput("Please Enter correct values");
-      setBtnChk(false);
-    } else {
-      setQuantity(quantity);
-    }
+    setQuantity(quantity);
   }
   function getCurrentPrice(event) {
     current = event.target.value;
-    if(current <= 0 || !current) {
-      setBtnChk(false);
-      setOutput("Please Enter correct values");
-    } else {
-      setCurrent(current);
-    }
+    setCurrent(current);
   }
   function clickHandler() {
-    if(!current || !purchase || !quantity) {
-      setOutput("Please Enter correct values");
+    if (
+      !current ||
+      !purchase ||
+      !quantity ||
+      current <= 0 ||
+      purchase <= 0 ||
+      quantity <= 0
+    ) {
+      setOutput("Please Enter the values properly");
+      graphics = "";
+      setGraphics(graphics);
+      theme = true;
+      setTheme(theme);
       return;
     }
     var difference = (current - purchase) * quantity;
@@ -54,12 +51,7 @@ export default function App() {
       purchase
     ).toFixed(2);
     console.log(differencePercentage);
-    if(isNaN(differencePercentage) || differencePercentage === undefined || isNaN(difference) || difference === undefined){
-      setOutput("Please Enter correct values" + quntaty + ": current values are not accaptable");
-      setBtnChk(false);
-    }
-    else if (differencePercentage < -50) {
-      setBtnChk(true);
+    if (differencePercentage < -50) {
       graphics = <img style={{ marginTop: "1rem" }} src={gif}></img>;
       setGraphics(graphics);
       setTimeout(() => {
@@ -76,7 +68,6 @@ export default function App() {
         setOutput(output);
       }, 1500);
     } else if (differencePercentage > 0) {
-      setBtnChk(true);
       graphics = <img style={{ marginTop: "1rem" }} src={gif}></img>;
       setGraphics(graphics);
       setTimeout(() => {
@@ -93,7 +84,6 @@ export default function App() {
         setOutput(output);
       }, 1500);
     } else if (differencePercentage < 0 && differencePercentage >= -50) {
-      setBtnChk(true);
       graphics = <img style={{ marginTop: "1rem" }} src={gif}></img>;
       setGraphics(graphics);
       setTimeout(() => {
@@ -110,7 +100,6 @@ export default function App() {
         setOutput(output);
       }, 1500);
     } else if (current === purchase) {
-      setBtnChk(true);
       graphics = <img style={{ marginTop: "1rem" }} src={gif}></img>;
       setGraphics(graphics);
       setTimeout(() => {
@@ -140,50 +129,52 @@ export default function App() {
         }}
       >
         <form>
-        <h1>Check Profit/Loss on your Stock</h1>
-        <div class="form__group">
-          <input
-            onChange={getPurchasePrice}
-            type="number"
-            min = "1"  
-            class="form__field"
-            placeholder="(₹)"
-            required
-          />
-          <label for="name" class="form__label">
-            Purchase Price (₹)
-          </label>
-        </div>
+          <h1>Check Profit/Loss on your Stock</h1>
+          <div class="form__group">
+            <input
+              onChange={getPurchasePrice}
+              type="number"
+              min="1"
+              class="form__field"
+              placeholder="(₹)"
+              required
+            />
+            <label for="name" class="form__label">
+              Purchase Price (₹)
+            </label>
+          </div>
 
-        <div class="form__group">
-          <input
-            onChange={getPurchaseQuantity}
-            type="number"
-            min = "1"
-            class="form__field"
-            placeholder="Quantity"
-            required            
-          />
-          <label for="name" class="form__label">
-            Purchase Quantity
-          </label>
-        </div>
+          <div class="form__group">
+            <input
+              onChange={getPurchaseQuantity}
+              type="number"
+              min="1"
+              class="form__field"
+              placeholder="Quantity"
+              required
+            />
+            <label for="name" class="form__label">
+              Purchase Quantity
+            </label>
+          </div>
 
-        <div class="form__group">
-          <input
-            onChange={getCurrentPrice}
-            type="number"
-            min = "1"
-            class="form__field"
-            placeholder="(₹)"
-            required
-          />
-          <label for="name" class="form__label">
-            Current price (₹)
-          </label>
-        </div>
+          <div class="form__group">
+            <input
+              onChange={getCurrentPrice}
+              type="number"
+              min="1"
+              class="form__field"
+              placeholder="(₹)"
+              required
+            />
+            <label for="name" class="form__label">
+              Current price (₹)
+            </label>
+          </div>
 
-        <button type="submit" disabled={btnChk} onClick={clickHandler}>check</button>
+          <button type="button" onClick={clickHandler}>
+            check
+          </button>
         </form>
       </div>
       <div className="result">
